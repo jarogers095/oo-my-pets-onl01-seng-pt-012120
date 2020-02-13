bf4e4ed8af2beb4f7a3eaefe340d12d8bbd3c246
James Rogers <jarogers095@gmail.com>
2020-02-13 05:01:16 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,16 @@
 class Owner
+  @@assert_select_email
   attr_reader(:name, :species)
   
   def initialize(owner_name)
     @name = owner_name
+    @species = "human"
   end
+  
+  def say_species()
+    puts @species
+  end
+  
   
   
 end
